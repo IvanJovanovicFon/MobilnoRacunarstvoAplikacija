2a42341b5090f78dac55f17638f2d27fb50574d3
Ivan Jovanovic <ij20190045@student.fon.bg.ac.rs>
2023-06-29 17:48:04 +0200

    Odradjen api na servisu i na favorites strani zato sto ne radi rutiranje

--- src/app/movies-tab/favorites/favorites.page.ts
+++ src/app/movies-tab/favorites/favorites.page.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieApiService } from '../movie-api.service';
+import { Movie } from '../movie.model';
+import { AxiosResponse } from 'axios';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.page.html',
   styleUrls: ['./favorites.page.scss'],
 })
-export class FavoritesPage implements OnInit {
+export class FavoritesPage {
+  searchTerm: string = '';
+  searchResults: any[] = [];
 
-  constructor() { }
+  constructor(private movieApiService: MovieApiService) {}
+  
 
-  ngOnInit() {
+  async search() {
+    try {
+      if (this.searchTerm.trim() !== '') {
+        const response = await this.movieApiService.searchMovies(this.searchTerm);
+        this.searchResults = response.results;
+        const movies: Movie[] = this.searchResults.map((result: any) => ({
+          id: result.id,
+          title: result.title,
+          year:result.release_date,
+          imageUrl: result.poster_path
+        }));
+        console.log(movies); 
+      } else {
+        this.searchResults = [];
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
-
 }
+
+
